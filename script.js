3dfa0e5723bff9d6d361bea1809e4b260d2528e2
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:20:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-$.get(
-    "https://9ced-122-161-50-113.in.ngrok.io/robot.png",
-    {paramOne : 1, paramX : 'abc'},
-    function(data) {
-       alert('page content: ' + data);
-    }
-);
+fetch('https://9ced-122-161-50-113.in.ngrok.io/robot.png')
