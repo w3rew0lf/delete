c1407429593112ddda7720d09a93e22a86725590
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:07:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-window.onload = function() {
-  console.log("Hello");
-  window.status = "tuhin1729";
-};
+document.getElementById('btn').onclick = function() {
+            var val = document.getElementById('imagename').value,
+                src = 'https://static.vecteezy.com/system/resources/previews/000/199/370/original/vector-robot-cheerful-isolated-on-blue-background-concept-illustration.jpg',
+                img = document.createElement('img');
+
+            img.src = src;
+            document.body.appendChild(img);
