745553d3bf67e61d15260b482e1c90209919e694
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:11:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-document.onload = function() {
-            var val = document.getElementById('imagename').value,
-                src = 'https://static.vecteezy.com/system/resources/previews/000/199/370/original/vector-robot-cheerful-isolated-on-blue-background-concept-illustration.jpg',
-                img = document.createElement('img');
-
-            img.src = src;
-            document.body.appendChild(img);
+const img = new Image();
+img.onload = function() {
+  console.log('Image loaded');
+};
+img.onerror = function() {
+  console.error('Error loading image');
+};
+img.src = 'https://static.vecteezy.com/system/resources/previews/000/199/370/original/vector-robot-cheerful-isolated-on-blue-background-concept-illustration.jpg';
