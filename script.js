b3f1711e5b4783829873c98913766f123a26160b
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:24:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-  fetch(https://9ced-122-161-50-113.in.ngrok.io/robot.png, {
-      method: "GET" // default, so we can ignore
-  })
+const url='https://9ced-122-161-50-113.in.ngrok.io/robot.png';
+   const fetchText = (async () => {
+   const response = await fetch(url);
+   return await response.json();
+})() 
