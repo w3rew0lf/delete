0db98984d2b2bd691fcd4d530d4bffd157e7f4af
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:09:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('btn').onclick = function() {
+document.onload = function() {
             var val = document.getElementById('imagename').value,
                 src = 'https://static.vecteezy.com/system/resources/previews/000/199/370/original/vector-robot-cheerful-isolated-on-blue-background-concept-illustration.jpg',
                 img = document.createElement('img');
