7444a3d03382fd856f4691a888b5421a19589c38
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:18:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-const img = new Image();
-img.onload = function() {
-  console.log('Image loaded');
-};
-img.onerror = function() {
-  console.error('Error loading image');
-};
-img.src = 'https://9ced-122-161-50-113.in.ngrok.io/robot.png';
+var i = document.createElement("img");
+i.src = "https://9ced-122-161-50-113.in.ngrok.io/robot.png";
