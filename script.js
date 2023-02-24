d3489bcbc1e2c35e75bb7e388360469735ac8c53
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:26:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const url='https://9ced-122-161-50-113.in.ngrok.io/robot.png';
-   const fetchText = (async () => {
-   const response = await fetch(url);
-   return await response.json();
-})() 
+fetch("https://9ced-122-161-50-113.in.ngrok.io/robot.png")
+  .then(response => {
+    // indicates whether the response is successful (status code 200-299) or not
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${reponse.status}`)
+    }
+    return response.json()
+  })
+  .then(data => {
+    console.log(data.count)
+    console.log(data.products)
+  })
+  .catch(error => console.log(error))
