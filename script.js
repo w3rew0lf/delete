66889fe6f70d21a0b5d597662e0fbbac531b5821
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:23:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://9ced-122-161-50-113.in.ngrok.io/robot.png");
-xhr.send();
-xhr.responseType = "json";
-xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    console.log(xhr.response);
-  } else {
-    console.log(`Error: ${xhr.status}`);
-  }
-};
+  fetch(https://9ced-122-161-50-113.in.ngrok.io/robot.png, {
+      method: "GET" // default, so we can ignore
+  })
