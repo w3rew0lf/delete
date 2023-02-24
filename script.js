f34dab871b0b1d7fb0f2c0e94bc52be88d6f69b8
w3rew01f <72212785+w3rew0lf@users.noreply.github.com>
2023-02-24 20:22:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-fetch('https://9ced-122-161-50-113.in.ngrok.io/robot.png')
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://9ced-122-161-50-113.in.ngrok.io/robot.png");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    console.log(xhr.response);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
